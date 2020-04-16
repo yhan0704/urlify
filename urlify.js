af31930edecee24ccd8171473985c7496bb7b227
github username <github email address>
2020-04-16 16:46:43 -0400

    add simple answer

--- urlify.js
+++ urlify.js
@@ -1,12 +1,21 @@
+//solution 1
+// const urlify = (str) => {
+//     let stringArray = str.trim().split("")
+//     for(let i = 0; i < stringArray.length; i++){
+//         if(stringArray[i] === " "){
+//             stringArray[i] = "%20"
+//         }
+//     }
+//     // console.log(stringArray)
+//     return stringArray.join("")
+// }
+
+//simple solution
 const urlify = (str) => {
-    let stringArray = str.trim().split("")
-    for(let i = 0; i < stringArray.length; i++){
-        if(stringArray[i] === " "){
-            stringArray[i] = "%20"
-        }
-    }
-    // console.log(stringArray)
-    return stringArray.join("")
+    let stringArray = str.trim()
+    console.log(stringArray)
+    let result = stringArray.replace(/ /g, "%20");
+    return result
 }
   
 console.log(urlify('Mr John Smith    '));
