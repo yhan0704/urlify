8ad55d3ac69f8bb8fb14714e195f65bca999aedd
github username <github email address>
2020-04-16 16:40:18 -0400

    done

--- urlify.js
+++ urlify.js
@@ -1,6 +1,13 @@
-const replaceUrlSpaces = (str) => {
-    
+const urlify = (str) => {
+    let stringArray = str.trim().split("")
+    for(let i = 0; i < stringArray.length; i++){
+        if(stringArray[i] === " "){
+            stringArray[i] = "%20"
+        }
+    }
+    // console.log(stringArray)
+    return stringArray.join("")
 }
   
-   console.log(replaceUrlSpaces("Sai Charan P"));
+console.log(urlify('Mr John Smith    '));
   
